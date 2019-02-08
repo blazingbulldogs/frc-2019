a6fbfb456de42cbe80d8000581173297e950188b
Jonah Snider <me@jonahsnider.ninja>
2019-02-07 16:43:54 -0800

    config: Adjust settings for testing

--- src/main/java/frc/robot/structures/Config.java
+++ src/main/java/frc/robot/structures/Config.java
@@ -4,13 +4,11 @@ package frc.robot.structures;
  * Settings used across the project.
  */
 public final class Config {
-  // Whether or not to use USB cameras
-  public static final boolean camerasEnabled = true;
-
   public static final String jetsonIP = "http://10.5.81.33";
 
   // Enable certain subsytems
   public static final boolean driveSubsytemEnabled = true;
   public static final boolean solenoidsSubsytemEnabled = true;
   public static final boolean gyroSubsytemEnabled = false;
+  public static final boolean camerasEnabled = false;
 }
