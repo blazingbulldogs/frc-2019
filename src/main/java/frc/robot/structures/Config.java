b3614be293e02060512583791b209bcb41cf190e
Jonah Snider <me@jonahsnider.ninja>
2019-02-09 11:52:19 -0800

    config: Update Jetson IP address

--- src/main/java/frc/robot/structures/Config.java
+++ src/main/java/frc/robot/structures/Config.java
@@ -4,7 +4,7 @@ package frc.robot.structures;
  * Settings used across the project.
  */
 public final class Config {
-  public static final String jetsonIP = "http://10.5.81.33";
+  public static final String jetsonIP = "http://10.5.81.19";
 
   // Enable certain subsytems
   public static final boolean driveSubsytemEnabled = true;
