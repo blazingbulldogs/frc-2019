89af454e786ef4c4b41d35225bb0e46d9a5b98d0
Nathaniel Zerai <nathangamz0@gmail.com>
2019-03-02 13:53:11 -0800

    Switch from random number generation to static tab "Driving"

--- src/main/java/frc/robot/structures/Logger.java
+++ src/main/java/frc/robot/structures/Logger.java
@@ -8,8 +8,8 @@ import frc.robot.RobotMap;
 
 public class Logger {
   // Use random number to create new tabs when debugging
-  public static final ShuffleboardTab tab = Shuffleboard.getTab(Double.toString(Math.random()));
-  // public static final ShuffleboardTab tab = Shuffleboard.getTab("Driving");
+  // public static final ShuffleboardTab tab = Shuffleboard.getTab(Double.toString(Math.random()));
+  public static final ShuffleboardTab tab = Shuffleboard.getTab("Driving");
   public static final DriveJoystick driveJoystick = new DriveJoystick(RobotMap.joystick);
   private final NetworkTableEntry joystickData = tab
       .add("Joystick Output", new double[] { 0, 0, 0 })
